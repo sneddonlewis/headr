0faf3dd625b6e788ee5a8710e11911648761e868
Lewis Sneddon <lewis@lewiscodes.io>
2023-12-28 11:09:27 +0000

    starter with error handling

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+use clap::{App, Arg};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("headr")
+        .version("0.1.0")
+        .author("Lewis Sneddon")
+        .about("Rust head")
+        .arg(
+            Arg::with_name("lines")
+                .short("n")
+                .long("lines")
+                .value_name("LINES")
+                .help("Number of lines")
+                .default_value("10"),
+        )
+        .get_matches();
+
+    Ok(Config {
+        files: Vec::new(),
+        lines: 0,
+        bytes: None,
+    })
+}
+
+pub fn run(_cfg: Config) -> MyResult<()> {
+    println!("hello world");
+    Ok(())
+}
