6ebba1120c93e92d03d79364244cb1b74374a777
Lewis Sneddon <lewis@lewiscodes.io>
2023-12-28 11:45:48 +0000

    parsed args

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
 use clap::{App, Arg};
 use std::error::Error;
 
@@ -23,16 +26,63 @@ pub fn get_args() -> MyResult<Config> {
                 .help("Number of lines")
                 .default_value("10"),
         )
+        .arg(
+            Arg::with_name("bytes")
+                .short("b")
+                .long("bytes")
+                .value_name("BYTES")
+                .help("Number of bytes"),
+        )
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILES")
+                .help("Input file(s)")
+                .default_value("-"),
+        )
         .get_matches();
 
+    let lines = matches
+        .value_of("lines")
+        .map(parse_positive_int)
+        .transpose()
+        .map_err(|e| format!("illegal line count -- {}", e))?;
+
+    let bytes = matches
+        .value_of("bytes")
+        .map(parse_positive_int)
+        .transpose()
+        .map_err(|e| format!("illegal byte count -- {}", e))?;
+
     Ok(Config {
-        files: Vec::new(),
-        lines: 0,
-        bytes: None,
+        files: matches.values_of_lossy("files").unwrap(),
+        lines: lines.unwrap(),
+        bytes: bytes,
     })
 }
 
-pub fn run(_cfg: Config) -> MyResult<()> {
-    println!("hello world");
+pub fn run(cfg: Config) -> MyResult<()> {
+    println!("{:#?}", cfg);
     Ok(())
+}
+
+fn parse_positive_int(val: &str) -> MyResult<usize> {
+    match val.parse() {
+        Ok(res) if res > 0 => Ok(res),
+        _ => Err(val.into()),
+    }
+}
+
+#[test]
+fn test_parse_positive_int() {
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
 }
